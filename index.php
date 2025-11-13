3c7f25c46d38197072897c7e0c2568db25d4fd33
XAYITBOYEV008 <xayitboyevdiyorbek21@gmail.comProfile imagexayitboyevdiyorbek21@gmail.com>
2025-11-13 16:05:50 +0500

    arifmetik amallar

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-echo "Salom";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Arifmetik amallar</title>
+</head>
+<body>
+  <?php
+$a = 10;
+$b = 23;
+echo "yigindi:".$a+$b."<br>" ;
+echo "ayrma:".$a-$b ."<br>";
+echo "bolnma:".$a/$b ."<br>";
+echo "kopaytma:".$a*$b."<br>" ;
+?>
+</body>
+</html>
