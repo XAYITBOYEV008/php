62aa5dd4ec367f1954551cb1734088af1cfb6dae
XAYITBOYEV008 <xayitboyevdiyorbek21@gmail.comProfile imagexayitboyevdiyorbek21@gmail.com>
2025-11-18 15:42:05 +0500

    switch keys

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Arifmetik amallar</title>
-</head>
-<body>
-  <?php
-$a = 10;
-$b = 23;
-echo "yigindi:".$a+$b."<br>" ;
-echo "ayrma:".$a-$b ."<br>";
-echo "bolnma:".$a/$b ."<br>";
-echo "kopaytma:".$a*$b."<br>" ;
-?>
-</body>
-</html>
+<?php
+$a = 1;
+switch ($a) {
+  case 1:
+    echo " bugun dushanba ";
+    break;
+  case "2":
+    echo "bugun seshanba";
+    break;
+   case "3":
+    echo "bugun chorshanba";
+    break;
+     case "4":
+    echo "bugun payshanba";
+    break;
+     case "5":
+    echo "bugun juma";
+    break;
+     case "6":
+    echo "bugun shanba";
+    break;
+     case "7":
+    echo "bugun yakshanba";
+    break;
+  default:
+    echo "bunday hafta kuni yoq";
+}
