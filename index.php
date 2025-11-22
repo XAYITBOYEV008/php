33c7781d18863db0cf2bdd51876b7528b5a53d28
whoami <whoami@kali.linux>
2025-11-22 13:21:59 -0500

    oddiy test1

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-<?php 
-$a = 67;
+<?php
+// Foydalanuvchidan GET orqali son qabul qilish, default = 67
+$a = isset($_GET['son']) ? (int)$_GET['son'] : 67;
 $e = $a ** 3;
 
 if ($a > 0 && $a < 10) {
-echo $e;
-}
-elseif ($a >= 10 && $a < 50) {
-    echo sqrt($a);
-}
-else {
-    echo "bunday son mavjud emas";
+    echo "Sonning kubi: " . $e;
+} elseif ($a >= 10 && $a < 50) {
+    echo "Sonning kvadrat ildizi: " . sqrt($a);
+} else {
+    echo "Bunday son mavjud emas";
 }
 ?>
+
