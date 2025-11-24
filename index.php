4cdb164912c8a79a91082cc42d717da36047c05a
XAYITBOYEV008 <xayitboyevdiyorbek21@gmail.com>
2025-11-24 14:25:35 +0500

    harajatlarga pulni hisoblash

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$a = 6 ;// taksiga
+$b = 7; // menga 
+$c = 4; // darga
+$e = 1; //ovqatga
+$h = 1211; // tel ga
+
+$yigindi = $a+$b+$c+$e+$h;
+echo "mendga shuncha  pul kerak:"." ".$yigindi
+?>
