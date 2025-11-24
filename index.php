f50f4b092c4402899f007e2520525da444abf86e
XAYITBOYEV008 <xayitboyevdiyorbek21@gmail.com>
2025-11-24 21:47:46 +0500

    html+php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
-$a = 6 ;// taksiga
-$b = 7; // menga 
-$c = 4; // darga
-$e = 1; //ovqatga
-$h = 1211; // tel ga
-
-$yigindi = $a+$b+$c+$e+$h;
-echo "mendga shuncha  pul kerak:"." ".$yigindi
+echo "<del>salom sunyo</del>"
 ?>
