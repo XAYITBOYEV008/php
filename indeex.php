797b89aa1a54f78ca14993703a1ab7bae867089c
XAYITBOYEV008 <xayitboyevdiyorbek21@gmail.com>
2025-12-12 18:00:48 +0500

    tup sunni topish

--- indeex.php
+++ indeex.php
@@ -1,5 +1,16 @@
-<?php 
+<?php
+// Tup son topish misoli
 
-echo "helllllllllllllloooooooooooooooooooo"
+$son = 17;
 
+// Dastlabki taxmin: tub
+$tub = $son > 1;
+
+// 2 dan ildizigacha bo‘linishlarni tekshirish
+for ($i = 2; $i * $i <= $son && $tub; $i++) {
+    if ($son % $i == 0) $tub = false;
+}
+
+// Natijani chiqarish
+echo $tub ? "Bu tub son: $son" : "Bu tub son emas: $son";
 ?>
